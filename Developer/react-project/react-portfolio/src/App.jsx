2dd281a14f976436552c5d13bcf33aeaec07c33b
Yuta Asai <asagon.ngzk46@gmail.com>
2024-09-04 10:24:08 +0900

    Updated

--- Developer/react-project/react-portfolio/src/App.jsx
+++ Developer/react-project/react-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
@@ -6,23 +7,26 @@ import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import SocialLinks from './components/SocialLinks';
-
-
+import Footer from './components/footer'
 
 function App() {
-
   return (
-    <>
-    <Navbar />
-    <Home />
-
-    <SocialLinks />
-    <About />
-    <Projects />
-    <Skills />
-    <Contact />
-    </>
+    <Router>
+      <div>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <SocialLinks />
+        {/* <Contact /> */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
